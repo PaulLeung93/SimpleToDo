0ef25c9aae087f4b72714d8dbe31aa93518290c4
Paul Leung <78509075+PaulLeung93@users.noreply.github.com>
2021-09-10 16:25:49 -0400

    Initial commit

---
+++ BeerAdviser/app/src/main/java/com/example/beeradviser/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.beeradviser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    //Instance of BeerExpert as private variable
+    private BeerExpert expert = new BeerExpert();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Called when the user clicks the button
+    public void onClickFindBeer(View view){
+
+        //Gets a reference to TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+        //Gets a reference to Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        List<String> brandList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+
+
+        for (String brand : brandList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+
+        brands.setText(brandsFormatted);
+    }
+}
